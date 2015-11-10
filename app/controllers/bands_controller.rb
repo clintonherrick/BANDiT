0399a8f3d3ef69dcc5c45055f973921458814cec
clintonherrick <clintonherrick@gmail.com>
2015-11-09 22:25:18 -0800

    prints band title and content

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,11 +1,12 @@
 class BandsController < ApplicationController
   before_action :find_band, only: [:show]
+
   def index
+    @bands = Band.all.order("created_at DESC")
   end
 
   def show
   end
-
 
   def new
     @band = Band.new
