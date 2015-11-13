e953a992e876bcc8331b2905ece436964999b064
Clinton <clintonherrick@gmail.com>
2015-11-13 12:00:01 -0800

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151113001003) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bands", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20151113001003) do
     t.datetime "image_updated_at"
   end
 
-  add_index "bands", ["user_id"], name: "index_bands_on_user_id"
+  add_index "bands", ["user_id"], name: "index_bands_on_user_id", using: :btree
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 20151113001003) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
